c80c5ce695773f5bcf8885571eac3bc010ecb1cb
PLAYPLAK <131369587+PLAYPLAK@users.noreply.github.com>
2023-10-05 01:05:15 +0700

    16

--- unit_test.py
+++ unit_test.py
@@ -5,8 +5,8 @@ from app import app
 class AppTestCase(unittest.TestCase):
     def test_hello_with_string(self):
         # test hello
-        res = app.hello("Mqaas")
-        self.assertEqual(res, "Hello, Mqaas")
+        res = app.hello("Mqas")
+        self.assertEqual(res, "Hello, Mqas")
 
     def test_hello_with_number(self):
         # test hello
